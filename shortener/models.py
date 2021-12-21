02ee49daaf4eaa43021ba7e72866837ac4ea9ed1
vicente97p4 <vicente97p4@naver.com>
2021-12-21 23:51:40 +0900

    form ¼öÁ¤

--- shortener/models.py
+++ shortener/models.py
@@ -5,37 +5,38 @@ from django.db import models
 from django.contrib.auth.models import User as U
 
 # Create your models here.
+
+
 class TimeStampedModel(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
-        abstract = True # django¥¬ ≈¨∑°Ω∫∏¶ ≈◊¿Ã∫Ì¿ª ∏∏µÈæÓº≠ ∞¸∏Æ«—¥Ÿ.
-        # ±◊∑°º≠ ¿Ã∑∏∞‘ ªÛº”Ω√≈∞¥¬ class∏¶ ∏∏µÂ∑¡∏È
-        # Meta classø°º≠ abstract = True∏¶ «ÿ¡‡æﬂ «—¥Ÿ.
+        abstract = True
 
 
 class PayPlan(TimeStampedModel):
     name = models.CharField(max_length=20)
     price = models.IntegerField()
-    
 
 
 class Organization(TimeStampedModel):
     class Industries(models.TextChoices):
-        PERSONAL = 'personal'
-        RETAIL = 'retail'
-        MANUFACTURING = 'manufacturing'
-        IT = 'it'
-        OTHERS = 'others'
-    name = models.CharField(max_length=50) # ?öå?Ç¨ ?ù¥Î¶?
-    industry = models.CharField(max_length=15, choices=Industries.choices, default=Industries.OTHERS) # ?úÑ 5Í∞? Ï§? 1Í∞?
+        PERSONAL = "personal"
+        RETAIL = "retail"
+        MANUFACTURING = "manufacturing"
+        IT = "it"
+        OTHERS = "others"
+
+    name = models.CharField(max_length=50)
+    industry = models.CharField(max_length=15, choices=Industries.choices, default=Industries.OTHERS)
     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING, null=True)
 
 
 class Users(models.Model):
     user = models.OneToOneField(U, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100, null=True)
+    url_count = models.IntegerField(default=0)
     organization = models.ForeignKey(Organization, on_delete=models.DO_NOTHING, null=True)
 
 
@@ -43,14 +44,14 @@ class EmailVerification(TimeStampedModel):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     key = models.CharField(max_length=100, null=True)
     verified = models.BooleanField(default=False)
-    
-    
+
+
 class Categories(TimeStampedModel):
     name = models.CharField(max_length=100)
     organization = models.ForeignKey(Organization, on_delete=models.DO_NOTHING, null=True)
     creator = models.ForeignKey(Users, on_delete=models.CASCADE)
-    
-    
+
+
 class ShortenedUrls(TimeStampedModel):
     class UrlCreatedVia(models.TextChoices):
         WEBSITE = "web"
@@ -60,12 +61,15 @@ class ShortenedUrls(TimeStampedModel):
         str_pool = string.digits + string.ascii_letters
         return ("".join([random.choice(str_pool) for _ in range(6)])).lower()
 
+    def rand_letter():
+        str_pool = string.ascii_letters
+        return random.choice(str_pool).lower()
+
     nick_name = models.CharField(max_length=100)
     category = models.ForeignKey(Categories, on_delete=models.DO_NOTHING, null=True)
-    prefix = models.CharField(max_length=50) # /1234 ?ùº?äî url?ù¥ ?ûà?ã§Í≥? ?ï¥Î≥¥Ïûê, Í∑∏Îüº 9999ÍπåÏ??Î∞ñÏóê ÎßåÎì§ ?àò ?óÜ?ã§. Í∑∏Îûò?Ñú ?ïû?óê ?ûÑ?ùò?ùò Î¨∏ÏûêÎ•? ?ëêÍ∏? ?úÑ?ï¥ ÎßåÎì§?óà?ã§. ?ÇòÏ§ëÏóê?äî Î¨∏Ïûê ????ã† ?õê?ïò?äî identityÎ•? ?Ñ£?äî?ã§.
+    prefix = models.CharField(max_length=50, default=rand_letter)
     creator = models.ForeignKey(Users, on_delete=models.CASCADE)
     target_url = models.CharField(max_length=2000)
     shortened_url = models.CharField(max_length=6, default=rand_string)
-    created_via = models.CharField(max_length=8, choices=UrlCreatedVia.choices, default=UrlCreatedVia.WEBSITE)
+    create_via = models.CharField(max_length=8, choices=UrlCreatedVia.choices, default=UrlCreatedVia.WEBSITE)
     expired_at = models.DateTimeField(null=True)
-    
